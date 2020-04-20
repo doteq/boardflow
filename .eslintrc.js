6ad5981b32da55653091e3243b7095ddab667a15
Dominik Korsa <dominik.korsa@gmail.com>
2020-04-20 21:43:17 +0200

    Configure linter

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    'vuetify',
+  ],
+  plugins: [
+    'vuetify',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    semi: ['error', 'always'],
+    'import/order': ['error'],
+    'import/prefer-default-export': 'off',
+    'vuetify/no-deprecated-classes': 'error',
+    'vuetify/grid-unknown-attributes': 'error',
+    'vuetify/no-legacy-grid': 'error',
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -38,23 +38,21 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <hello-world />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+    data: () => ({
     //
-  }),
-};
+    }),
+  };
 </script>
