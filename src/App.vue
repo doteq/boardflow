ea33350a604233a1d6a21c401b9d24e776a64a32
Dominik Korsa <dominik.korsa@gmail.com>
2020-05-25 23:12:24 +0200

    Add development and production projects

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,24 @@
       <router-view />
     </v-content>
     <cookies-toast />
+    <development-info v-if="developmentMode" />
   </v-app>
 </template>
 
 <script>
   import CookiesToast from './components/CookiesToast.vue';
+  import DevelopmentInfo from './components/DevelopmentInfo.vue';
 
   export default {
     name: 'App',
     components: {
       CookiesToast,
+      DevelopmentInfo,
+    },
+    computed: {
+      developmentMode () {
+        return process.env.NODE_ENV === 'development';
+      },
     },
   };
 </script>
---
+++ src/components/DevelopmentInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="development-info">
+    <v-alert
+      icon="mdi-code-tags"
+      class="ma-0"
+      dense
+      color="secondary"
+      text
+      border="left"
+    >
+      {{ $t('development-mode') }}
+    </v-alert>
+  </v-card>
+</template>
+
+<style scoped>
+  .development-info {
+    position: fixed;
+    bottom: 8px;
+    left: 8px;
+  }
+</style>
